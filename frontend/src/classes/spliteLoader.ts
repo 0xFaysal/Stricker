42e7062bb1c729a2e25658ac38b1145abcf422d5
Faysal Ahmed Fahim <153068178+0xFaysal@users.noreply.github.com>
2025-07-20 03:05:56 +0600

    Add sprite data for stickman and related animations

---
+++ frontend/src/classes/spliteLoader.ts
@@ -0,0 +1,32 @@
+export const spriteData = [
+    {
+        spriteKey: 'stickman',
+        filename: 'stickman.png',
+        cellSize: { x: 64, y: 64 },
+        offset: { x: -32, y: -62 }
+    },
+    {
+        spriteKey: 'stickmanR',
+        filename: 'stickmanR.png',
+        cellSize: { x: 64, y: 64 },
+        offset: { x: -32, y: -62 }
+    },
+    {
+        spriteKey: 'stickmanAttacks',
+        filename: 'stickmanAttacks.png',
+        cellSize: { x: 128, y: 64 },
+        offset: { x: -96, y: -62 }
+    },
+    {
+        spriteKey: 'stickmanAttacksR',
+        filename: 'stickmanAttacksR.png',
+        cellSize: { x: 128, y: 64 },
+        offset: { x: -32, y: -62 }
+    },
+    {
+        spriteKey: 'stickmanShadow',
+        filename: 'stickmanShadow.png',
+        cellSize: { x: 64, y: 32 },
+        offset: { x: -32, y: -16 }
+    }
+];
